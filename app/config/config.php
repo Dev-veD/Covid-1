ec5f6ed7f4de270866895063cea53e02b412a3c7
maniSHarma7575 <39980734+maniSHarma7575@users.noreply.github.com>
2020-04-18 15:08:40 +0530

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -3,15 +3,15 @@
 //Database Parameters
 
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','Divya');
+define('DB_USER','admin');
+define('DB_PASS','password');
 define('DB_NAME','covid');
 
 //approot
 define('APPROOT',dirname(dirname(__FILE__)));
 
 //URLROOT
-define('URLROOT','');
+define('URLROOT','https://amdcr.co.in/');
 define('DS', DIRECTORY_SEPARATOR);
 
 //sitename
