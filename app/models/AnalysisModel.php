e604dc2bcc59ad9dbb673d4941212e5e4500e333
maniSHarma7575 <39980734+maniSHarma7575@users.noreply.github.com>
2020-04-20 13:37:13 +0530

    Updated Requirements

--- app/models/AnalysisModel.php
+++ app/models/AnalysisModel.php
@@ -37,6 +37,48 @@
             return false;
         }
     }
+    public function editAnalysisFile($data)
+    {
+        
+        $this->db->query('UPDATE analysis SET description=:description,type=:type,document_path=:document_path WHERE id=:id');
+    $this->db->bindvalues(':description',$data['description']);
+    $this->db->bindvalues(':type',$data['type']);
+    $this->db->bindvalues(':document_path',$data['document_path']);
+    $this->db->bindvalues(':id',$data['id']);
+
+  
+        if($this->db->execute())
+        {
+          
+          return true;
+        }
+        else
+        {
+          
+          return false;
+        }
+    }
+    public function editAnalysis($data)
+    {
+        
+        $this->db->query('UPDATE analysis SET description=:description,type=:type WHERE id=:id');
+    $this->db->bindvalues(':description',$data['description']);
+    $this->db->bindvalues(':type',$data['type']);
+    $this->db->bindvalues(':id',$data['id']);
+
+  
+        if($this->db->execute())
+        {
+          
+          return true;
+        }
+        else
+        {
+          
+          return false;
+        }
+    }
+    
 }
 
 ?>
