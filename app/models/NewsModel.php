e37010e22580ea3e45ffa579addc487029ace6b2
maniSHarma7575 <39980734+maniSHarma7575@users.noreply.github.com>
2020-04-20 13:38:05 +0530

    Updated Requirements

--- app/models/NewsModel.php
+++ app/models/NewsModel.php
@@ -18,13 +18,13 @@
   }
   public function addNewNews($data)
   {
-    $this->db->query('INSERT INTO news(title,date,portal,edition,page,weblink) values(:title,:date,:portal,:edition,:page,:weblink)');
+    $this->db->query('INSERT INTO news(title,date,description,category,document_path,filetype) values(:title,:date,:description,:category,:document_path,:filetype)');
     $this->db->bindvalues(':title',$data['title']);
     $this->db->bindvalues(':date',$data['date']);
-    $this->db->bindvalues(':portal',$data['portal']);
-    $this->db->bindvalues(':edition',$data['edition']);
-    $this->db->bindvalues(':page',$data['page']);
-    $this->db->bindvalues(':weblink',$data['weblink']);
+    $this->db->bindvalues(':description',$data['description']);
+    $this->db->bindvalues(':category',$data['category']);
+    $this->db->bindvalues(':document_path',$data['attachment']);
+    $this->db->bindvalues(':filetype',$data['filetype']);
     if($this->db->execute())
     {
         return true;
@@ -40,7 +40,53 @@
     $this->db->bindvalues(':id', $id);
     $this->db->execute();
   }
+  public function editNews($data)
+  {
+    
+    $this->db->query('UPDATE news SET title=:title,date=:date,description=:description,category=:category WHERE id=:id');
+    $this->db->bindvalues(':title',$data['title']);
+    $this->db->bindvalues(':date',$data['date']);
+    $this->db->bindvalues(':description',$data['description']);
+    $this->db->bindvalues(':category',$data['category']);
+    $this->db->bindvalues(':id',$data['id']);
 
+  
+        if($this->db->execute())
+        {
+          
+          return true;
+        }
+        else
+        {
+          
+          return false;
+        }
+
+  }
+  public function editNewsFile($data)
+  {
+    
+    $this->db->query('UPDATE news SET title=:title,date=:date,description=:description,category=:category,document_path=:document_path WHERE id=:id');
+    $this->db->bindvalues(':title',$data['title']);
+    $this->db->bindvalues(':date',$data['date']);
+    $this->db->bindvalues(':description',$data['description']);
+    $this->db->bindvalues(':category',$data['category']);
+    $this->db->bindvalues(':document_path',$data['attachment']);
+    $this->db->bindvalues(':id',$data['id']);
+
+  
+        if($this->db->execute())
+        {
+          
+          return true;
+        }
+        else
+        {
+          
+          return false;
+        }
+
+  }
   
 }
 
